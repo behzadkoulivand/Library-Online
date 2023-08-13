7fa4dbc15b039078ae741c349e004edbab0a6c9f
Behzad Koulivand <behzadk.1245@gmail.com>
2023-08-13 13:30:35 +0330

    UserModel writed

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+
+const sequelize = require('../config/database');
+
+const User = sequelize.define("User", {
+    userCode: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    fullName:{
+        type:DataTypes.STRING,
+        allowNull: false
+    }, 
+    password:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    }, 
+    phone:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    // books: {
+
+    // }
+});
+
+module.exports = User;
