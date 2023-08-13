d1dc9fb727d24e7492ecabd4d07a5b2ae423a4b3
Behzad Koulivand <behzadk.1245@gmail.com>
2023-08-13 13:14:03 +0330

    Connecred to mySQL db

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
+const sequelize = require('./config/database');
+
 const app = express();
 
 const PORT = process.env.POTR || 3000;
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+sequelize.sync().then((result) => {
+    console.log("mySQL Connected!");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((err) => console.log(err));
