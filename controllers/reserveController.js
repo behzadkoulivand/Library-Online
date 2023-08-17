c5d315beb552452dd8829d96f73bf8796fa21a48
Behzad Koulivand <behzadk.1245@gmail.com>
2023-08-17 16:31:39 +0330

    Return Book writed

--- controllers/reserveController.js
+++ controllers/reserveController.js
@@ -21,11 +21,37 @@ exports.borrowBook = async (req, res, next) => {
             user: req.userCode,
             book: bookId
         });
-        
+
         book.reserved = true;
         book.reservator = req.userCode;
         book.save();
         res.status(200).json(["کتاب زیر برای شما رزرو شد", book]);
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.returnBook = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+
+        const borrow = await Reserve.findOne({ where: { book: id } }) || await Reserve.findByPk(id);
+
+        if (!borrow) {
+            const error = new Error("امانت مورد نظر یافت نشد");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        const bookId = borrow.book;
+        const book = await Book.findByPk(bookId);
+        book.reserved = false;
+        book.reservator = 0;
+        book.save();
+
+        await Reserve.destroy({ where: { id } });
+        res.status(200).json("کتاب مورد نظر برگشت داده شد");
 
     } catch (err) {
         next(err);
